beb5e4aa693fc111524682bc32a3027288750263
Steve Perkins <steve@indabamusic.com>
2013-03-25 13:52:16 -0400

    tests for following methods
    tests for follow, unfollow, loadFollowers, loadFollowing

--- test/fixtures/server.js
+++ test/fixtures/server.js
@@ -4,17 +4,44 @@ var MAX_OFFSET = parseInt(process.env.MAX_OFFSET, 10);
 var express = require('express');
 var app = express();
 
-function fixtureResponse(req, resp) {
+app.use(function(req, resp, next) {
   if (req.query.offset && req.query.offset >= MAX_OFFSET) {
     resp.json(require('./empty.json'));
   }
   else {
-    resp.json(require('.' + req.path));
+    next();
   }
+});
+
+function fixtureResponse(req, resp) {
+  resp.json(require('.' + req.path));
+}
+
+function okResponse(req, resp) {
+  resp.json({status: 'success'});
 }
 
 app.get('/users', fixtureResponse);
 app.get('/whoami', fixtureResponse);
+
+app.get('/users/:user_id/follows', function(req, resp) {
+  resp.json(require('./users'));
+});
+app.get('/users/:user_id/followers', function(req, resp) {
+  resp.json(require('./users'));
+});
+
+app.post('/users/:user_id/follow', okResponse);
+app.post('/users/:user_id/unfollow', okResponse);
+
+app.get('*', function(req, res){
+  console.error('\nno GET:', req.path);
+  res.send(404);
+});
+app.post('*', function(req, res){
+  console.error('\nno POST:', req.path);
+  res.send(404);
+});
 
 app.listen(port);
 console.log('test server on ' + port);
--- test/unit.js
+++ test/unit.js
@@ -6,13 +6,17 @@ var assert = require('assert');
 var testServer = require('./fixtures/server');
 
 var client;
+var fixture = {
+  users: require('./fixtures/users').data
+};
 
-beforeEach(function() {
+beforeEach(function(done) {
   client = indabaClient({
     dorianEndpoint: 'http://beta.indavelopment.com',
     lydianEndpoint: 'http://localhost:' + port,
     token: 'test-token'
   });
+  client.whoami(done);
 });
 
 describe('client.get', function() {
@@ -22,8 +26,9 @@ describe('client.get', function() {
       cast: client.User
     };
     client.get(req, function(err, data) {
+      assert.ifError(err);
       assert.ok(data[0] instanceof client.User);
-      done(err);
+      done();
     });
   });
   it('all option gets all the records', function(done) {
@@ -33,18 +38,81 @@ describe('client.get', function() {
       all: true
     };
     client.get(req, function(err, data) {
+      assert.ifError(err);
       assert.equal(data.length, MAX_OFFSET);
-      done(err);
+      done();
     });
   });
 });
+
 
 describe('whoami', function() {
   it('sets currentUser', function(done) {
     client.whoami(function(err, currentUser) {
-      assert.ok(client.currentUser);
-      done(err);
+      assert.ifError(err);
+      assert.equal(currentUser, client.currentUser);
+      done();
     });
   });
 });
 
+
+describe('client.loadFollowing', function() {
+  it('populates client.following', function(done) {
+    client.loadFollowing(function(err, following) {
+      assert.ifError(err);
+      assert.ok(following);
+      assert.equal(client.following.length, following.length);
+      done();
+    });
+  });
+});
+
+
+describe('client.follow', function() {
+  var someUser = fixture.users[0];
+  it('adds user to following', function(done) {
+    assert.ok(!client.isFollowing(someUser));
+    var beforeLength = client.following.length;
+    client.follow(someUser, function(err) {
+      assert.ifError(err);
+      assert.equal(client.following.length, beforeLength + 1);
+      assert.ok(client.isFollowing(someUser));
+      done();
+    });
+  });
+});
+
+
+describe('client.unfollow', function() {
+  var someUser = fixture.users[8];
+  var beforeLength;
+  beforeEach(function(done) {
+    client.follow(someUser, function(err) {
+      assert.ifError(err);
+      assert.ok(client.isFollowing(someUser));
+      beforeLength = client.following.length;
+      done();
+    });
+  });
+  it('removes user from following', function(done) {
+    client.unfollow(someUser, function(err) {
+      assert.ifError(err);
+      assert.equal(client.following.length, beforeLength - 1);
+      assert.ok(!client.isFollowing(someUser));
+      done();
+    });
+  });
+});
+
+
+describe('client.loadFollowers', function() {
+  it('populates client.followers', function(done) {
+    client.loadFollowers(function(err, data) {
+      assert.ifError(err);
+      var datum = data[0];
+      assert.ok(client.isFollowedBy(datum));
+      done();
+    });
+  });
+});
